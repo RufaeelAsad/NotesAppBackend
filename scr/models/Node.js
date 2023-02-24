50072c69dcddcb3994f8a8acc158e2c0cd3c410e
Rufaeel Asad <rufaeelasadmac@Rufaeels-MacBook-Air.local>
2023-02-25 01:25:06 +0500

    Debuging Api issue

--- scr/models/Node.js
+++ scr/models/Node.js
@@ -1,32 +1,23 @@
 const mongoose = require("mongoose");
 
 const noteschema = mongoose.Schema({
-    id: {
-        type: String,
-        unique: true,
-        required: true,
-    },
+
 
     userid: {
         type: String,
-        required: true,
+        unique: true,
+        required: true
     },
 
     tittle: {
         type: String,
-        required: true,
     },
 
     content: {
         type: String,
-    
+
     },
-
-    dateAdded: {
-        type: Date,
-        default: Date.now,
-
-    }
 });
 
-module.exports = mongoose.model("Note", noteschema);
+
+module.exports = mongoose.model("Node", noteschema);
--- scr/server.js
+++ scr/server.js
@@ -2,7 +2,7 @@
 // Initializing
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 const mongoose = require('mongoose');
 const Note = require('./models/Node');
 const bodyParser = require("body-parser");
@@ -16,17 +16,17 @@ app.use(bodyParser.json());
 mongoose.connect("mongodb+srv://rufaeelasad:5D3A7zCNXzGgwUwi@cluster0.vj8rauc.mongodb.net/notesdb").then(() => {
 
 
-app.get('/', (req, res) => {
+    // for home page
+    app.get('/', (req, res) => {
 
-    const response = { message: "THis is home Page"};
-    res.json(response);
-    
-})
+        const response = { message: "THis is home Page" };
+        res.json(response);
 
-
-
+    });
 
 
+
+    // show all list api
     app.get('/notes/list', async (req, res) => {
 
         const allNotes = await Note.find();
@@ -38,6 +38,7 @@ app.get('/', (req, res) => {
 
 
 
+    // search specific note
     app.post('/notes/list/search', async (req, res) => {
 
         const specificNote = await Note.find({ userid: req.body.userid });
@@ -45,14 +46,17 @@ app.get('/', (req, res) => {
         res.json(specificNote);
     });
 
+
+
+    // adding a new note api
     app.post('/notes/add', async (req, res) => {
 
 
-        await Note.deleteOne({ id: req.body.id });
+        await Note.deleteOne({ userid: req.body.userid });
 
 
         const addNote = new Note({
-            id: req.body.id,
+
             userid: req.body.userid,
             tittle: req.body.tittle,
             content: req.body.content
@@ -62,16 +66,17 @@ app.get('/', (req, res) => {
 
         const response = { message: "New Note Created" };
 
-        res.json(req.body);
+        res.json(response);
 
     });
 
-
+    
+    // delete api
     app.post('/notes/delete', async (req, res) => {
 
-        await Note.deleteOne({ id: req.body.id });
+        await Note.deleteOne({ userid: req.body.userid });
 
-        const response = { message: "Note has been created" };
+        const response = { message: "Note has been deleted" };
         res.json(response);
 
     });
@@ -83,7 +88,7 @@ app.get('/', (req, res) => {
 
 
 // Starting the server on Port
-app.listen(PORT, () => {
+app.listen(4000, () => {
 
     console.log("Server run successfully");
 
